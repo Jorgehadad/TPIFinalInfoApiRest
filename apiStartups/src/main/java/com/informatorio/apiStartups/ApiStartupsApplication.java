5fc93a7dd9ed8bdce4772eb352979919747d6c1e
Jorgehadad <jorgemenegaz@gmail.com>
2021-12-12 20:25:23 -0300

    iniciando el proyecto con spring boot

---
+++ apiStartups/src/main/java/com/informatorio/apiStartups/ApiStartupsApplication.java
@@ -0,0 +1,13 @@
+package com.informatorio.apiStartups;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiStartupsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiStartupsApplication.class, args);
+	}
+
+}
